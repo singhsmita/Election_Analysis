9bf751db7e4727c654d5d1fa2f6fd206dc9ab4c1
Smita Singh <119648166+singhsmita@users.noreply.github.com>
2023-01-06 20:26:39 -0500

    Add files via upload

--- PyPoll.py
+++ PyPoll.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 
 # add your dependencies
 import csv
@@ -91,34 +91,4 @@ print(winning_candidate_summary)
 
 
 
-=======
 
-#add your dependencies
-import csv
-import os
-
-#Assign a variable to load a file from a path.
-file_to_load = os.path.join("Resources", "election_results.csv")
-
-# Create a filename variable to a direct or indirect path to the file.
-file_to_save = os.path.join("analysis", "election_analysis.txt")
-
-# Open the election results and read the file.
-with open(file_to_load) as election_data:
-    file_reader = csv.reader(election_data)
-
-   # Print the header row.
-    headers=next(file_reader)
-    print(headers)
-
-    #Print each row in CSV file.
-
-    for row in file_reader:
-        print(row)
-
-
-
-
-
-
->>>>>>> 1dd7c1ee057c57d690dc704ff1ec35a6e18539b0
